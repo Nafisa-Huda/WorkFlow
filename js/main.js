14f2bfe58bfcfa58e3e6673e00be5c4af1b0aaec
Nh0000 <94311576+Nh0000@users.noreply.github.com>
2022-08-03 11:13:08 -0400

    Added quote API

--- js/main.js
+++ js/main.js
@@ -12,3 +12,22 @@ list.forEach((clickedList)=>{
         // document.body.style.background=clickedTabBGColor;
     });
 });
+
+
+//motivational quotes
+window.addEventListener('load', getFetch)
+function getFetch(){
+  //const choice = document.querySelector('input').value
+  const url = 'https://free-quotes-api.herokuapp.com/' 
+  fetch(url)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        console.log(data)
+        document.querySelector('.quote').innerText = data.quote
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+}
+
+const calendar = document.querySelector('.calendar')
