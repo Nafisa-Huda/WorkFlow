754a152ce769e7f0629383d8ade799cd348797cb
Nh0000 <94311576+Nh0000@users.noreply.github.com>
2022-08-06 18:52:07 -0400

    Added comments to main js

--- js/main.js
+++ js/main.js
@@ -15,15 +15,14 @@ list.forEach((clickedList)=>{
 
 
 //motivational quotes
-window.addEventListener('load', getFetch)
+window.addEventListener('load', getFetch) //when the window loads the getFetch function is called which makes an HTTP request to the API server  
 function getFetch(){
-  //const choice = document.querySelector('input').value
-  const url = 'https://free-quotes-api.herokuapp.com/' 
+  const url = 'https://free-quotes-api.herokuapp.com/' //quotes api url
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
-        document.querySelector('.quote').innerText = data.quote
+        document.querySelector('.quote').innerText = data.quote //displays the quote on the page
       })
       .catch(err => {
           console.log(`error ${err}`)
@@ -31,35 +30,35 @@ function getFetch(){
 }
 
 
-
+//Calendar
 const date = new Date();
 
 const renderCalendar = () => {
   date.setDate(1);
 
-  const monthDays = document.querySelector(".calendar-days");
+  const monthDays = document.querySelector(".days");
 
-  const lastDay = new Date(
+  const lastDayOfMonth = new Date(
     date.getFullYear(),
     date.getMonth() + 1,
     0
-  ).getDate();
+  ).getDate(); //Gets last day of current month
 
-  const prevLastDay = new Date(
+  const prevlastDayOfMonth = new Date(
     date.getFullYear(),
     date.getMonth(),
     0
-  ).getDate();
+  ).getDate(); //Gets the last day of the previous month 
 
-  const firstDayIndex = date.getDay();
+  const firstDayOfMonthIndex = date.getDay(); //index number of the first day of the month
 
-  const lastDayIndex = new Date(
+  const lastDayOfMonthIndex = new Date(
     date.getFullYear(),
     date.getMonth() + 1,
     0
-  ).getDay();
+  ).getDay(); //returns index of last day of the month
 
-  const nextDays = 7 - lastDayIndex - 1;
+  const nextDays = 7 - lastDayOfMonthIndex - 1; //returns 
 
   const months = [
     "January",
@@ -76,41 +75,38 @@ const renderCalendar = () => {
     "December",
   ];
 
-  document.querySelector(".date h1").innerHTML = months[date.getMonth()];
+  document.querySelector(".date h1").innerHTML = months[date.getMonth()]; //Uses month array and getMonth method to display current Month
 
-  document.querySelector(".date p").innerHTML = new Date().toDateString();
+  document.querySelector(".date p").innerHTML = new Date().toDateString(); //Displays current date in a readable format
 
   let days = "";
 
-  for (let x = firstDayIndex; x > 0; x--) {
-    days += `<div class="prev-date">${prevLastDay - x + 1}</div>`;
-  }
+  for (let x = firstDayOfMonthIndex; x > 0; x--) {
+    days += `<div class="prev-date">${prevlastDayOfMonth - x + 1}</div>`;
+  } //For the previous month dates in the calendar 
 
-  for (let i = 1; i <= lastDay; i++) {
-    if (
-      i === new Date().getDate() &&
-      date.getMonth() === new Date().getMonth()
-    ) {
-      days += `<div class="today">${i}</div>`;
+  for (let i = 1; i <= lastDayOfMonth; i++) {
+    if (i === new Date().getDate() && date.getMonth() === new Date().getMonth()) {
+     days += `<div class="today">${i}</div>`;
     } else {
       days += `<div>${i}</div>`;
     }
   }
 
-  for (let j = 1; j <= nextDays; j++) {
-    days += `<div class="next-date">${j}</div>`;
-    monthDays.innerHTML = days;
+  for (let n = 1; n <= nextDays; n++) {
+    days += `<div class="next-date">${n}</div>`; //content for the next days
   }
+  monthDays.innerHTML = days; //displays days for the next month
 };
 
-document.querySelector(".prev").addEventListener("click", () => {
-  date.setMonth(date.getMonth() - 1);
-  renderCalendar();
+document.querySelector(".prev").addEventListener("click", () => { //click event listener to render the PREVIOUS month 
+  date.setMonth(date.getMonth() - 1); //set the month as the current month minus one -> previous month
+  renderCalendar(); //call the global render calendar function
 });
 
-document.querySelector(".next").addEventListener("click", () => {
-  date.setMonth(date.getMonth() + 1);
-  renderCalendar();
+document.querySelector(".next").addEventListener("click", () => { //click event listener to render the NEXT month 
+  date.setMonth(date.getMonth() + 1); //set the month as the current month plus one -> next month
+  renderCalendar(); //call the global render calendar function
 });
 
-renderCalendar();
+renderCalendar(); //function to render the calendar
