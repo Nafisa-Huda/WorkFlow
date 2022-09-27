3aa94f32a0032f50073acf90c2f4355a5da1ea9d
Nafisa <nafisahuda12@gmail.com>
2022-09-27 17:43:24 -0400

    Passport for local authentication

--- config/database.js
+++ config/database.js
@@ -1,15 +1,20 @@
 const mongoose = require('mongoose')
+require("dotenv").config({ path: "./config/.env" });
 
 const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(
-            process.env.DB_CONNECTION
-        )
-        console.log(`MongoDB connected: ${conn.connection.host}`)
-    } catch (err) {
-        console.log(err)
-        process.exit(1)
-    }
+  try {
+    const conn = await mongoose.connect(process.env.DB_STRING, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      // useFindAndModify: false,
+      // useCreateIndex: true
+    })
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`)
+  } catch (err) {
+    console.error(err)
+    process.exit(1)
+  }
 }
 
 module.exports = connectDB
