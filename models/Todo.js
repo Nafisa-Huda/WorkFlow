759c2753461250760d96cc94d1c70ebbc468ffc8
Nafisa <nafisahuda12@gmail.com>
2022-09-27 17:41:58 -0400

    Todo model

---
+++ models/Todo.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+
+const TodoSchema = new mongoose.Schema({
+  todo: {
+    type: String,
+    required: true,
+  },
+  todoDescription: {
+    type: String,
+    required: true,
+  },
+  dueDate: {
+    type: String,    
+    required: true,
+  },
+  priority:{
+    type: String,
+    required: true,
+  },
+  listType:{
+    type: String,
+    required: true,
+  },
+  completed: {
+    type: Boolean,
+    required: true,
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true
+  }
+})
+
+module.exports = mongoose.model('Todo', TodoSchema)
