4e2a6c75ec6e0de31a7d16417d38b50f432f08d8
Nafisa <nafisahuda12@gmail.com>
2022-10-17 14:23:51 -0400

    fixed module error

--- controllers/events.js
+++ controllers/events.js
@@ -1,4 +1,4 @@
-const Event = require("../models/Event");
+const Event = require("../models/event");
 const startOfDay = require("date-fns/startOfDay");
 const endOfDay = require("date-fns/endOfDay");
 const subDays = require("date-fns/subDays");
